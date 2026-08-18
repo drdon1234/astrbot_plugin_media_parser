c1a1a5ecee9dbe8b344777be05d048e539925b08
FFFold <yu_daa@hotmail.com>
2026-08-18 09:02:43 +0800

    fix: 规避 getChild 子日志器触发 AstrBot 日志格式化崩溃

--- core/logger.py
+++ core/logger.py
@@ -3,12 +3,7 @@
 try:
     from astrbot.api import logger as _astrbot_logger
 
-    get_child = getattr(_astrbot_logger, "getChild", None)
-    logger = (
-        get_child("astrbot_plugin_media_parser")
-        if callable(get_child)
-        else _astrbot_logger
-    )
+    logger = _astrbot_logger
 except ImportError:
     import logging
 
